4b09ca7bd62dad94899a99ee9804e27560d184c6
Ranjeetkum <85489633+Ranjeetkum@users.noreply.github.com>
2021-12-09 11:11:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+function simpleInterestCalculator()
+{
+    let p = document.getElementById("pa").value;
+    let r = document.getElementById("rate").value;
+    let t = document.getElementById("time").value;
+    let result = (p*r*t/100);
+    
+    //console.log(result);
+    document.getElementById("result").innerHTML= "Simple Interest  " + result;
+}
+
+function resetcalculator()
+{
+    // document.getElementsByClassName("reset")[0].value="";
+    // document.getElementsByClassName("reset")[1].value="";
+    for (let i=0; i< document.getElementsByClassName("reset").length ; i++)
+    {
+        document.getElementsByClassName("reset")[i].value="";
+    }
+    document.getElementById("result").innerText= "";
+}
